525fca336fccb2e30cc5798ba56edb0a4d04e4a5
TakenakaHitoshi <hitoshidayo0111@gmail.com>
2019-11-13 11:13:25 +0900

    課題提出 PHP - データ型

---
+++ taskDataType.php
@@ -0,0 +1,24 @@
+<?php
+  $count= 5;
+  $string= "string型を使用するときに、ダブルクオート(\")を表示する場合は、バックスラッシュ(\)が必要です。";
+  $intro='I\'m Security Enginner';
+  $name= "太郎くん";
+  const GREETINGNIGHT = "こんばんは";
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>taskDataType</title>
+</head>
+<body>
+    <p><?php echo("りんごが".$count."個あります。"); ?></p>
+    <p><?php echo $string;?></p>
+    <p><?php echo $intro;?></p>
+    <p><?php echo(GREETINGNIGHT."、".$name); ?></p>
+</body>
+<body>
+</body>
+</html>
