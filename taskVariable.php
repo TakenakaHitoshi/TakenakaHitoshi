b46fcd7183b9702fc0e893baf66f4ef57804322f
TakenakaHitoshi <hitoshidayo0111@gmail.com>
2019-11-13 11:12:26 +0900

    再提出

--- taskVariable.php
+++ taskVariable.php
@@ -1,3 +1,14 @@
+<?php
+    $name = "太郎くん";
+    const GREETINGNIGHT = "こんばんは、";
+    $applePrice = 110;
+    $sumAppleprice = $applePrice * 15;
+    $orangePrice=80;
+    $sumOrangeprice=$orangePrice*12;
+    $sumPrice=($sumAppleprice+$sumOrangeprice);
+    $sumpriceTax=$sumPrice*1.1;
+    $sumfruit=15+12;
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -7,36 +18,11 @@
     <title>taskVariable</title>
 </head>
 <body>
-<?php
-    $name = "太郎くん";
-    const GREETINGNIGHT = "こんばんは、";
-?>
- <p><?php echo(GREETINGNIGHT.$name); ?></p>
-
-<?php
-    $applePrice = 110;
-    $sumAppleprice = $applePrice * 15;
-?>
-<p><?php echo("りんごの値段：".$sumAppleprice);?></p>
-
-<?php
-    $orangePrice=80;
-    $sumOrangeprice=$orangePrice*12;
-?>
-<p><?php echo("みかんの値段:".$sumOrangeprice);?></p>
-<?php
-    $sumPrice=($sumAppleprice+$sumOrangeprice);
-?>
-<p><?php echo("合計金額（税抜）:".$sumPrice);?></p>
-
-<?php
-    $sumpriceTax=$sumPrice*1.1;
-?>
-<p><?php echo("合計金額（税込）:".$sumpriceTax);?></p>
-<?php
-    $sumfruit=15+12;
-?>
-<p><?php echo("フルーツ１個当たりの金額:".round($sumPrice/$sumfruit));?></p>
-
+　　<p><?php echo(GREETINGNIGHT.$name); ?></p>
+　　<p><?php echo("りんごの値段：".$sumAppleprice);?></p>
+　　<p><?php echo("みかんの値段:".$sumOrangeprice);?></p>
+　　<p><?php echo("合計金額（税抜）:".$sumPrice);?></p>
+　　<p><?php echo("合計金額（税込）:".$sumpriceTax);?></p>
+　　<p><?php echo("フルーツ１個当たりの金額:".round($sumPrice/$sumfruit));?></p>
 </body>
 </html>
