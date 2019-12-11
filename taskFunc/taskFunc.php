ca562f49b429ea5023aadad489ad91ea287dd2fb
TakenakaHitoshi <hitoshidayo0111@gmail.com>
2019-12-11 15:38:12 +0900

    再提出　PHP　関数

--- taskFunc/taskFunc.php
+++ taskFunc/taskFunc.php
@@ -7,33 +7,33 @@
     <title>taskFunc</title>
 </head>
 <body>
-    <p>課題１</p>
+<p>課題１</p>
     <?php
         function display($string){
-        echo($string. "<br>");
+            echo($string . "<br>");
         }
         display("関数で文字列を表示！");
-        display("254×322=". 254*322);
+        display("254×322=". 254 * 322);
     ?>
-    <p>課題２</p>
+<p>課題２</p>
     <?php
         function createRandomNumberArray(){
             for ($i= 0;$i<10;$i++){
              $array[$i]= rand(1,100);
-            } 
-            return $array;
+            }
+                return $array;
         }
         echo("<pre>");
         var_dump(createRandomNumberArray());
         echo("</pre>");
     ?>
-    <p>課題３</p>
+<p>課題３</p>
     <?php
         function createRandomNumberArrayAndDisplay(){
             for ($i= 1;$i<11;$i++){
                 $arrayNumber = rand(1,100);
-                display($i. "番目の数値：".$arrayNumber);
-               } 
+                display($i . "番目の数値：" . $arrayNumber);
+            } 
         }
         createRandomNumberArrayAndDisplay();
     ?>
@@ -51,26 +51,26 @@
         74,
         28
     ];
-    function numberAverageCalc(array $data, int $int = 0){     
+    function numberAverageCalc(array $data, int $int = 0){
         $total = 0;
         foreach ($data as $value){
-            $total += $value;           
+            $total += $value;
         };
-        $average = $total/count($data);
-        return round($average,$int);
+        $average = $total / count($data);
+            return round($average,$int);
     }
-    display("平均は（少数点1位は四捨五入）". numberAverageCalc($data)."です！");
-    display("平均は（少数点2位は四捨五入）". numberAverageCalc($data,2)."です！");
+    display("平均は（少数点1位は四捨五入）" . numberAverageCalc($data) . "です！");
+    display("平均は（少数点2位は四捨五入）" . numberAverageCalc($data,2) . "です！");
     ?>
     <p>課題４・２</p>
     <?php
     $result = 0;
     foreach($data as $value){
         if($value >= numberAverageCalc($data)){
-            $result += 1 ;  
+            $result += 1;
         }
     }
-    display("平均以上は".$result."個です！");
+    display("平均以上は" . $result . "個です！");
     ?>
 </body>
 </html>
