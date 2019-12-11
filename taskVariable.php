06d0ae23e6058311f01e26369c0249216d5c6828
TakenakaHitoshi <hitoshidayo0111@gmail.com>
2019-12-11 15:14:01 +0900

    再々提出　PHP PHP 定数と変数

--- taskVariable.php
+++ taskVariable.php
@@ -3,11 +3,11 @@
     const GREETINGNIGHT = "こんばんは、";
     $applePrice = 110;
     $sumAppleprice = $applePrice * 15;
-    $orangePrice=80;
-    $sumOrangeprice=$orangePrice*12;
-    $sumPrice=($sumAppleprice+$sumOrangeprice);
-    $sumpriceTax=$sumPrice*1.1;
-    $sumfruit=15+12;
+    $orangePrice = 80;
+    $sumOrangeprice = $orangePrice * 12;
+    $sumPrice = ($sumAppleprice + $sumOrangeprice);
+    const TAX  = 1.1;
+    $sumfruit = 15 + 12;
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -18,11 +18,11 @@
     <title>taskVariable</title>
 </head>
 <body>
-　　<p><?php echo(GREETINGNIGHT.$name); ?></p>
-　　<p><?php echo("りんごの値段：".$sumAppleprice);?></p>
-　　<p><?php echo("みかんの値段:".$sumOrangeprice);?></p>
-　　<p><?php echo("合計金額（税抜）:".$sumPrice);?></p>
-　　<p><?php echo("合計金額（税込）:".$sumpriceTax);?></p>
-　　<p><?php echo("フルーツ１個当たりの金額:".round($sumPrice/$sumfruit));?></p>
+　　<p><?php echo(GREETINGNIGHT . $name); ?></p>
+　　<p><?php echo("りんごの値段：" . $sumAppleprice . "円");?></p>
+　　<p><?php echo("みかんの値段:" . $sumOrangeprice . "円");?></p>
+　　<p><?php echo("合計金額（税抜）:" . $sumPrice . "円");?></p>
+　　<p><?php echo("合計金額（税込）:" . $sumPrice * TAX . "円");?></p>
+　　<p><?php echo("フルーツ１個当たりの金額:" . round($sumPrice / $sumfruit) . "円");?></p>
 </body>
 </html>
