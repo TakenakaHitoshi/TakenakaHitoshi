36e7554b2de745503f2451585368d1529288c3da
TakenakaHitoshi <hitoshidayo0111@gmail.com>
2019-12-16 18:56:14 +0900

    課題提出PHPーデータ挿入

---
+++ DB/insert.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>taskCalc</title>
+</head>
+<body>
+<p>課題１</p>
+<p>NSERT into `country` VALUES</p>
+<p>(1,'日本','アジア','日本語',81),</p>
+<p>(2,'イタリア','ヨーロッパ','イタリア語',39),</p>
+<p>(3,'アメリカ 合衆国','北アメリカ','英語',1),</p>
+<p>(4,'フランス','ヨーロッパ','フランス語',33),</p>
+<p>(5,'イギリス','ヨーロッパ','英語',44);</p>
+
+<p>課題２</p>
+<p>INSERT into `heritage` VALUES</p>
+<p>(1,'自由の女神像', 3,'文化'),</p>
+<p>(2,'小笠原諸島', 1,'自然'),</p>
+<p>(3,'姫路城', 1,'文化'),</p>
+<p>(4,'ロンドン塔', 5,'文化'),</p>
+<p>(5,'ナポリ歴史地区', 2,'文化'),</p>
+<p>(6,'富士山', 1,'自然'),</p>
+<p>(7,'ヴェルサイユの宮殿と庭園', 4,'文化');</p>
+
+</body>
+</html>
