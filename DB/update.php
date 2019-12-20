010dff9a03e6a841ed0cc4ed07eb21558835b01e
TakenakaHitoshi <hitoshidayo0111@gmail.com>
2019-12-20 20:53:08 +0900

    課題提出PHPーphpーデータ更新

---
+++ DB/update.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>taskCalc</title>
+</head>
+<body>
+<p>課題１</p>
+<p>1</p>
+<p>update country set area = '東アジア' where area = 'アジア';
+select * from country where area = 'アジア';</p>
+<p>2</p>
+<p>select * from heritage where `name` = '富士山'and `type` = '自然';
+update heritage set name = '富士山ー信仰の対象と芸術の源泉', type = '文化' where id = 6;</p>
